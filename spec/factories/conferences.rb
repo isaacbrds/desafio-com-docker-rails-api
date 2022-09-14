0fc383e7dcd94cd783463237fc34f3508cccfb32
isaacbrds <isaac.brigido@gmail.com>
2022-09-14 09:33:05 -0300

    Projeto - configurando ActiveStorage

--- spec/factories/conferences.rb
+++ spec/factories/conferences.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :conference do
     sequence(:title) { |n| "Conference #{n}" }    
+    file { Rack::Test::UploadedFile.new(Rails.root.join("spec/support/files/arq.txt")) }
   end
 end
--- spec/models/conference_spec.rb
+++ spec/models/conference_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Conference, type: :model do
-  it {is_expected.to validate_presence_of(:title)}
+  it { is_expected.to validate_presence_of(:title) }
+  it { is_expected.to validate_presence_of(:file) }
 end
